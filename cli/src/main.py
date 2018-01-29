332993ab524ed9332fc7211ee20a94f5ddd891be
Samir Talwar <samir@noodlesandwich.com>
2018-01-29 12:06:57 +0000

    CLI: Make a request to run the command. Patch the server to accept it.

--- cli/src/main.py
+++ cli/src/main.py
@@ -1,4 +1,5 @@
 import argparse
+import requests
 import sys
 
 
@@ -9,12 +10,39 @@ class RunCommand:
     def prepare_argument_parser(parser):
         parser.add_argument('command')
 
-    def __init__(self, host, command):
-        self.host = host
+    def __init__(self, host, port, command):
+        self.prefix = f'http://{host}:{port}'
         self.command = command
 
     def run(self):
-        print(self.command)
+        print(self.url('/'))
+        self.issue_command()
+
+    def issue_command(self):
+        response = requests.post(self.url('/commands'), json={
+            'command': self.command,
+        })
+        self.check_status(response, requests.codes.accepted)
+        print(response.json())
+
+    def check_status(self, response, expected_status):
+        if response.status_code != expected_status:
+            raise RequestException(response)
+
+    def url(self, path):
+        return self.prefix + path
+
+
+class RequestException(Exception):
+    def __init__(self, response):
+        try:
+            body = response.json()
+        except ValueError:
+            body = response.text
+        super().__init__(
+            f'Request failed with status code {response.status_code}.\n' +
+            f'Response:\n{body}'
+        )
 
 
 COMMANDS = {
@@ -24,7 +52,8 @@ COMMANDS = {
 
 def main(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host')
+    parser.add_argument('--host', default='localhost')
+    parser.add_argument('--port', type=int, default=80)
     subparsers = parser.add_subparsers(title='commands', dest='command_name')
     for name, command in COMMANDS.items():
         subparser = subparsers.add_parser(name, help=command.__doc__)
