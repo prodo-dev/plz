3e6f445ef269a673d9bc64c1161b1c23841cf4d0
Samir Talwar <samir@noodlesandwich.com>
2018-03-20 13:31:21 +0000

    VPC: Remove the security group that exposes SSH. Use the VPN instead.

--- machines/vpc/main.tf
+++ machines/vpc/main.tf
@@ -32,28 +32,3 @@ resource "aws_default_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_security_group" "ssh" {
-  vpc_id      = "${data.aws_vpc.main.id}"
-  name        = "Plz SSH"
-  description = "Allow SSH and Mosh access"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 60000
-    to_port     = 61000
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name  = "Plz SSH"
-    Owner = "Infrastructure"
-  }
-}
