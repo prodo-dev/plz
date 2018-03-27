3b509c51ef3a0b090b23e1f011c814c9e7f1445e
Samir Talwar <samir@noodlesandwich.com>
2018-03-27 22:16:36 +0100

    Controller: Fix deployment to pass in the new configuration.

--- vars.mk
+++ vars.mk
@@ -11,6 +11,7 @@ AWS_REGION = eu-west-1
 AWS_AVAILABILITY_ZONE = eu-west-1a
 AWS_PROJECT = 024444204267.dkr.ecr.eu-west-1.amazonaws.com
 AMI_TAG = 2018-03-27
+AWS_WORKER_AMI = plz-worker-$(AMI_TAG)
 KEY_NAME = plz-$(shell tr '[:upper:]' '[:lower:]' <<< $(ENVIRONMENT_NAME))-key
 DOMAIN = prodo.ai
 INTERNAL_DOMAIN = inside.$(DOMAIN)
@@ -24,7 +25,7 @@ bash:
 	@ echo 'export AWS_REGION="$(AWS_REGION)"'
 	@ echo 'export AWS_AVAILABILITY_ZONE="$(AWS_AVAILABILITY_ZONE)"'
 	@ echo 'export AWS_PROJECT="$(AWS_PROJECT)"'
-	@ echo 'export AWS_WORKER_AMI="plz-worker-$(AMI_TAG)"'
+	@ echo 'export AWS_WORKER_AMI="$(AWS_WORKER_AMI)"'
 	@ echo 'export AWS_KEY_NAME="$(KEY_NAME)"'
 
 .PHONY: terraform
