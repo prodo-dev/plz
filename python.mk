0c90a077384785203f9ff48d53fda856143f1a24
Samir Talwar <samir@noodlesandwich.com>
2018-03-28 01:49:32 +0100

    CLI/Controller: Use `pipenv` instead of requirements.txt.
    Advantages:
      - `pipenv` will manage the virtualenv for you
      - separates dependencies from transient dependencies cleanly
      - just run `pipenv update` to update all dependencies
    
    Disadvantages:
      - it's one more thing
    
    Instructions:
      1. Delete the two *env* directories.
      2. Follow the *README*.

--- python.mk
+++ python.mk
@@ -1,36 +1,13 @@
 SHELL := zsh -e -u
 
-PYTHON_EXE = python3.6
-PYTHON = $(shell command -v $(PYTHON_EXE) 2> /dev/null)
-SITE_PACKAGES = env/lib/$(PYTHON_EXE)/site-packages
-
-ifndef PYTHON
-$(error "Could not find $(PYTHON_EXE).")
-endif
-
 .PHONY: dist
-dist: $(SITE_PACKAGES)
+dist: environment
 	rm -rf build dist
-	python setup.py bdist_wheel
+	pipenv run python setup.py bdist_wheel
 
-.PHONY: site-packages
-site-packages: $(SITE_PACKAGES)
+.PHONY: environment
+environment: Pipfile.lock
+	pipenv install --keep-outdated --dev
 
-$(SITE_PACKAGES): env requirements.txt
-	./env/bin/pip install --requirement=requirements.txt
-	touch $@
-
-.PHONY: upgrade-python-dependencies
-upgrade-python-dependencies:
-	packages=($$(pip list --outdated --format=json | jq -r '.[] | .name')); \
-	if [[ $${#packages} -gt 0 ]]; then \
-	  pip install --upgrade $${packages[@]}; \
-	fi
-	./env/bin/pip freeze | grep -v '^pkg-resources=' > requirements.txt
-
-freeze: env
-	./env/bin/pip freeze | grep -v '^pkg-resources=' > requirements.txt
-
-env:
-	virtualenv --python=$(PYTHON_EXE) env
-	touch -t 200001010000 $(SITE_PACKAGES)
+Pipfile.lock: Pipfile
+	pipenv lock
