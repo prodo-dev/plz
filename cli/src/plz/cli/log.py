ff5fdc00f33f94b6552b4c1bff44cd9c36cdcf6f
Samir Talwar <samir@noodlesandwich.com>
2018-04-04 19:36:54 +0100

    CLI: [Fix] Print newlines at the end of log lines even without a TTY.

--- cli/src/plz/cli/log.py
+++ cli/src/plz/cli/log.py
@@ -10,13 +10,15 @@ def log_info(message):
         print('\x1b[32m', end='')
     print(message, end='')
     if sys.stdout.isatty():
-        print('\x1b[0m')
+        print('\x1b[0m', end='')
+    print()
 
 
 def log_error(message):
     isatty = sys.stdout.isatty()
     if isatty:
         print('\x1b[31m', end='')
-    print('❗' if isatty else '!  ', message, end='')
+    print('!! ' if isatty else '!  ', message, end='')
     if isatty:
-        print('\x1b[0m')
+        print('\x1b[0m', end='')
+    print()
