7b8cba97c2e486aacc301dd9f5005f5f14030fd6
Samir Talwar <samir@noodlesandwich.com>
2018-04-06 17:38:12 +0100

    Test: Fix the download-input test.

--- test/04-download-input.ignored/main.py
+++ test/04-download-input.ignored/main.py
@@ -9,9 +9,15 @@ with open(os.environ['CONFIGURATION_FILE']) as c:
 input_directory = config['input_directory']
 output_directory = config['output_directory']
 
-for filename in os.listdir(input_directory):
-    with open(os.path.join(input_directory, filename)) as input_file:
-        with open(os.path.join(output_directory, filename)) as output_file:
+files = (os.path.join(directory, file)
+         for (directory, _, files) in os.walk(input_directory)
+         for file in files)
+for input_file_path in files:
+    filename = os.path.relpath(input_file_path, input_directory)
+    output_file_path = os.path.join(output_directory, filename)
+    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+    with open(input_file_path, 'r') as input_file:
+        with open(output_file_path, 'w') as output_file:
             contents: str = input_file.read()
             output_file.write(contents.upper())
     print(filename)
