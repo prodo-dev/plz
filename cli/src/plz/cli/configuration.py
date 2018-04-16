f6f921923ba1d1942dcb78858a65d3a4e3106f00
sergiogiro <sergiogiro@users.noreply.github.com>
2018-04-16 18:03:33 +0100

    Revert the default docker runtime to None

--- cli/src/plz/cli/configuration.py
+++ cli/src/plz/cli/configuration.py
@@ -79,7 +79,7 @@ class Configuration:
             # the current work directory)
             Property('included_paths', type=list, default=[]),
             Property('debug', type=bool, default=False),
-            Property('docker_runtime', type=str, default='nvidia'),
+            Property('docker_runtime', type=str, default=None),
         ]
     }
 
