582320b209807733908662e879b99b129556e249
Samir Talwar <samir@noodlesandwich.com>
2018-04-18 01:36:01 +0100

    Controller: Speed up the build by bypassing the Docker client.
    I don't know why it's so much slower, but it is.

--- services/controller/src/plz/controller/images/local.py
+++ services/controller/src/plz/controller/images/local.py
@@ -1,22 +1,46 @@
 from typing import BinaryIO, Iterator
 
 import docker
+import docker.utils
+import requests
+import requests_unixsocket
 
 from plz.controller.images.images_base import Images
 
 
 class LocalImages(Images):
-    def __init__(self, docker_api_client: docker.APIClient, repository: str):
+    def __init__(self,
+                 docker_host: str,
+                 docker_api_client: docker.APIClient,
+                 repository: str):
         super().__init__(repository)
+        if not docker_host:
+            self.docker_host = 'http+unix://%2Fvar%2Frun%2Fdocker.sock'
+        else:
+            self.docker_host = docker.utils.parse_host(docker_host)
         self.docker_api_client = docker_api_client
 
     def build(self, fileobj: BinaryIO, tag: str) -> Iterator[str]:
-        return self.docker_api_client.build(
-            fileobj=fileobj,
-            custom_context=True,
-            encoding='bz2',
-            rm=True,
-            tag=f'{self.repository}:{tag}')
+        """
+        Builds an image from the tarball supplied as ``attr:fileobj``.
+
+        We used to use the Docker client to build the image, but for some
+        reason it's much slower, with a minimum of 5 seconds to build anything
+        under testing. Going direct seems to be much faster.
+        """
+        tag = f'{self.repository}:{tag}'
+        with requests_unixsocket.monkeypatch():
+            return requests.post(
+                self.docker_host + '/build',
+                params={
+                    't': tag,
+                },
+                headers={
+                    'Content-Type': 'application/tar',
+                    'Content-Encoding': 'bz2',
+                },
+                data=fileobj,
+            )
 
     def for_host(self, docker_url: str) -> 'LocalImages':
         new_docker_api_client = docker.APIClient(base_url=docker_url)
