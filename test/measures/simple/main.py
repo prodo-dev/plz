a0f4a52dc828b5cbde02681e2c9a60aab46dabed
Sergio Giro <sergio@prodo.ai>
2018-05-26 14:31:25 +0100

    Revert test

--- test/measures/simple/main.py
+++ test/measures/simple/main.py
@@ -3,14 +3,10 @@
 import json
 import os.path
 
-from time import sleep
-
 with open(os.environ['CONFIGURATION_FILE']) as c:
     config = json.load(c)
 
 summary_measures_file_name = config["summary_measures_file_name"]
-for i in range(10000):
-    with open(summary_measures_file_name, 'w') as f:
-        json.dump({'loss': 0.42-0.01*i}, f)
-    sleep(1)   
+with open(summary_measures_file_name, 'w') as f:
+    json.dump({'loss': 0.42}, f)
 
