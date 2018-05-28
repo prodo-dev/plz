e1d90304ebea333c576c4f09a4ebcf161e17a403
Samir Talwar <samir@noodlesandwich.com>
2018-05-28 19:27:48 +0100

    CLI: Keep the metaprogramming in `Server` simple.

--- cli/src/plz/cli/server.py
+++ cli/src/plz/cli/server.py
@@ -1,3 +1,4 @@
+import functools
 from typing import Sequence
 
 import requests
@@ -8,10 +9,17 @@ from plz.cli.configuration import Configuration
 from plz.cli.exceptions import CLIException
 
 
-def _method(name: str):
-    def impl(self: 'Server', *path_segments: str, **kwargs) -> Response:
+class Server:
+    @staticmethod
+    def from_configuration(configuration: Configuration):
+        return Server(configuration.host, configuration.port)
+
+    def __init__(self, host: str, port: int):
+        self.prefix = f'http://{host}:{port}'
+
+    def request(self, method: str, *path_segments: str, **kwargs) -> Response:
         try:
-            return requests.request(name, self._url(path_segments), **kwargs)
+            return requests.request(method, self._url(path_segments), **kwargs)
         except (ConnectionError,
                 requests.ConnectionError,
                 urllib3.exceptions.NewConnectionError) as e:
@@ -21,24 +29,13 @@ def _method(name: str):
             raise CLIException(
                 'Our connection to the server timed out.') from e
 
-    return impl
-
-
-class Server:
-    @staticmethod
-    def from_configuration(configuration: Configuration):
-        return Server(configuration.host, configuration.port)
-
-    def __init__(self, host: str, port: int):
-        self.prefix = f'http://{host}:{port}'
-
-    delete = _method('DELETE')
-    get = _method('GET')
-    head = _method('HEAD')
-    options = _method('OPTIONS')
-    patch = _method('PATCH')
-    post = _method('POST')
-    put = _method('PUT')
+    delete = functools.partialmethod(request, 'DELETE')
+    get = functools.partialmethod(request, 'GET')
+    head = functools.partialmethod(request, 'HEAD')
+    options = functools.partialmethod(request, 'OPTIONS')
+    patch = functools.partialmethod(request, 'PATCH')
+    post = functools.partialmethod(request, 'POST')
+    put = functools.partialmethod(request, 'PUT')
 
     def _url(self, path_segments: Sequence[str]) -> str:
         return self.prefix + '/' + '/'.join(path_segments)
