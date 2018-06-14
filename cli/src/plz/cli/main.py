219073c2bcdf09811556d8c9689eb0a8e1552c96
Sergio Giro <sergio@prodo.ai>
2018-06-14 12:13:58 +0100

    Informative error when no operation is specified

--- cli/src/plz/cli/main.py
+++ cli/src/plz/cli/main.py
@@ -53,6 +53,9 @@ def main(args=sys.argv[1:]):
         sys.exit(2)
 
     operation_name = options.operation_name
+    if operation_name is None:
+        parser.print_help()
+        sys.exit(2)
     option_dict = options.__dict__
     del option_dict['operation_name']
 
