732e27c52a95a7d94ea8311abaa2044591241282
Sergio Giro <sergio@prodo.ai>
2018-06-25 13:25:51 +0100

    Reverting tests upload inadvertently

--- test/logs/streaming/main.py
+++ test/logs/streaming/main.py
@@ -3,7 +3,7 @@
 import sys
 import time
 
-n = int(sys.argv[1]) if len(sys.argv) > 1 else 1000
+n = int(sys.argv[1]) if len(sys.argv) > 1 else 10
 
 for i in range(n):
     print(i, flush=True)
--- test/measures/simple/main.py
+++ test/measures/simple/main.py
@@ -2,14 +2,11 @@
 
 import json
 import os.path
-import time
 
 with open(os.environ['CONFIGURATION_FILE']) as c:
     config = json.load(c)
 
 summary_measures_path = config["summary_measures_path"]
-for i in range(1000):
-    with open(summary_measures_path, 'w') as f:
-        json.dump({'loss': 0.42 - i*0.0001}, f)
-    time.sleep(1)
+with open(summary_measures_path, 'w') as f:
+    json.dump({'loss': 0.42}, f)
 
