1b1fb98cbe0f2954b47f08ed8bb97570bf0f5e44
Sergio Giro <sergio@prodo.ai>
2018-07-17 16:17:12 +0100

    Python example working

---
+++ examples/just_python/main.py
@@ -0,0 +1,26 @@
+import sys
+import time
+
+#######################################
+# Minimal plz example using only python
+#######################################
+# We are trying to learn the value k for the function
+# f(x) = k*x
+# minimising
+# (x - 3 * f(x))^2 .
+# (Shhh, I'll tell you a secret: it's 1/3.)
+
+num_iterations = int(sys.argv[1])
+
+data = [900, 15, 27, 10, 1, 0.0003, -270, 33]
+
+k = 0
+for i in range(0, num_iterations):
+    print('k: ', k)
+    x = data[i % len(data)]
+    loss = pow(x - 3 * k * x, 2)
+    update = -6 * pow(x, 2) + 18 * k * pow(x, 2)
+    k = k - 0.0001/x * update
+    print('loss: ', loss)
+    print('update: ', update)
+    time.sleep(0.5)
