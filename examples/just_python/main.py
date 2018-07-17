317b5305cbee0c97e910479b3435cf2a10a5aa83
Sergio Giro <sergio@prodo.ai>
2018-07-17 17:18:09 +0100

    Write output

--- examples/just_python/main.py
+++ examples/just_python/main.py
@@ -19,8 +19,11 @@ num_iterations = int(sys.argv[1])
 OutputDirectory = str
 InputDirectory = str
 
+
 # You need to read the input from where plz left it, and write to places where
 # plz will pick it up.
+#
+# We write a function as to obtain the input and output dirs
 
 
 def get_configuration() -> Tuple[InputDirectory, OutputDirectory]:
@@ -48,22 +51,38 @@ def get_values_for_x(input_directory: InputDirectory) -> list:
         return json.load(f)
 
 
+def write_model(output_directory: OutputDirectory, k: float):
+    """We write "our model" (which happens to be a single value).
+
+       In real life it can be a neural network we obtained, or matrices, or
+       whatever we want to save."""
+    output_file = os.path.join(output_directory, 'model.json')
+    with open(output_file, 'w') as f:
+        json.dump({'k': k}, f)
+
+
 def main():
     input_directory, output_directory = get_configuration()
 
-    print('We are in the quest of finding a mysterious value for k.',
-          flush=True)
-    time.sleep(5)
-    print('The value happens to be 1/3, but don\'t tell anyone.',
-          flush=True)
+    # print('We are in the quest of finding a mysterious value for k.',
+    #       flush=True)
+    # time.sleep(5)
+    # print('The value happens to be 1/3, but don\'t tell anyone.',
+    #       flush=True)
+    # time.sleep(2)
     k = 0.0
 
     values_for_x = get_values_for_x(input_directory)
 
+    best_loss_so_far = 1000
     for i in range(0, num_iterations):
         print(f'k: {k:.4}', flush=True)
         x = values_for_x[i % len(values_for_x)]
         loss = pow(x - 3 * k * x, 2)
+        if loss < best_loss_so_far:
+            print('Best model so far! Saving')
+            best_loss_so_far = loss
+            write_model(output_directory, k)
         update = -6 * pow(x, 2) + 18 * k * pow(x, 2)
         # Weigh the learning rate by x
         k = k - 0.0001/x * update
