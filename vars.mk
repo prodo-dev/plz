8a2e5c4dd6e4f04f00f9d9d970055522d6917d36
Sergio Giro <sergio@prodo.ai>
2018-09-06 16:15:29 +0100

    Add default for BUILD_TIMESTAMP

--- vars.mk
+++ vars.mk
@@ -14,13 +14,22 @@ AMI_TAG = 2018-07-05
 # - A file called BUILD_TIMESTAMP is created in the root source of the
 #   controller in the controller image so that the controller can know its
 #   version
-# - Used by the setup.py of the cli to set the version of the wheel created
+# - The controller reports its version during ping
+# - Used by the setup.py of the cli to set the version of the package
+#   created
+# - The cli queries the version of the package it's running as to compare
+#   with the one reported by the controller
 #
 # There is also the file "STABLE_BUILD_TIMESTAMP". This relates only to
 # "non-plz-developers" that use a prebuilt version of plz.
 # The timestamp in this file should correspond to a build that is
-# "stable enough". The scripts to install the cli and to start the controller
-# use this timestamp as to refer to a wheel for the cli, and an image for the
-# controller, that have been already built.
+# "stable enough". The scripts to install the cli and to start the
+# controller use this timestamp as to refer to a pip package for the cli,
+# and an image for the controller, that have been already built.
 
-BUILD_TIMESTAMP = $(shell cat $(SECRETS_DIR)/BUILD_TIMESTAMP)
+BUILD_TIMESTAMP = $(shell cat $(SECRETS_DIR)/BUILD_TIMESTAMP 2> /dev/null || true)
+
+ifeq ($(BUILD_TIMESTAMP),)
+    BUILD_TIMESTAMP = 0
+endif
+
