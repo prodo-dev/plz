d402d02bb1efb291a872014dbc94280e6a1aee17
Sergio Giro <sergio@prodo.ai>
2018-11-26 16:06:03 +0000

    Controller tests: extract methods, add types

--- test/controller/src/test_parallel_indices.py
+++ test/controller/src/test_parallel_indices.py
@@ -1,14 +1,14 @@
 import json
 import math
-import time
 import unittest
 from collections import defaultdict
-from typing import Tuple, Optional
+from typing import Dict, Optional, Tuple
+
+import time
 
 from plz.cli.run_execution_operation import create_instance_market_spec
-
-from .utils import run_example, harvest, create_file_map_from_tarball, \
-    get_execution_listing_status, rerun_execution, TestingContext
+from .utils import TestingContext, create_file_map_from_tarball, \
+    get_execution_listing_status, harvest, rerun_execution, run_example
 
 
 class TestParallelIndices(unittest.TestCase):
@@ -86,74 +86,84 @@ class TestParallelIndices(unittest.TestCase):
         for index, subcomp in indices_to_compositions.items():
             # Json indices are always strings...
             index = int(index)
-
-            index_execution = subcomp['execution_id']
-
-            execution_listing_status = get_execution_listing_status(
-                context.controller, index_execution)
-            while harvest_after_run and execution_listing_status is not None:
-                if execution_listing_status is not 'running':
-                    context.controller.harvest()
-                time.sleep(0.1)
-                execution_listing_status = get_execution_listing_status(
-                    context.controller, index_execution)
-
+            index_execution_id = subcomp['execution_id']
             n_indices_of_execution = len(
-                execution_ids_to_indices[index_execution])
+                execution_ids_to_indices[index_execution_id])
 
-            logs_bytes = context.controller.get_logs(
-                index_execution, since=None)
-            logs_str = str(b''.join(logs_bytes), 'utf-8')
-            logs_lines = logs_str.split('\n')
-            # Make sure there's a newline at the end...
-            self.assertEqual(logs_lines[-1], '')
-            # ... and remove the empty line created by split
-            logs_lines = logs_lines[:-1]
-            self.assertEqual(len(logs_lines), n_indices_of_execution)
-            self.assertTrue(not all(
-                [line != str(index) for line in logs_lines]))
-
-            file_map = create_file_map_from_tarball(
-                context.controller.get_output_files(
-                    index_execution, index=index, path=None))
-
-            self.assertDictEqual(file_map, {'the_file': f'index is: {index}'})
-
-            measures = json.loads(''.join(context.controller.get_measures(
-                index_execution, summary=False, index=index)))
-            self.assertDictEqual(measures, {
-                'accuracy': index,
-                'summary': {
-                    'time': index
-                }})
-
-            summary_measures = json.loads(
-                ''.join(context.controller.get_measures(
-                    index_execution, summary=True, index=index)))
-            self.assertDictEqual(summary_measures, {'time': index})
-
-            # When not harvesting after run, check that the history works
-            history = context.controller.get_history(
-                user=context.configuration.user,
-                project=context.configuration.project)
-            # If we harvested, the execution is in the history, we can check
-            # the metadata
-            if harvest_after_run:
-                metadata = json.loads(''.join(history))[index_execution]
-                # Check that the number of in
-                self.assertEqual(len(metadata['measures'].keys()),
-                                 n_indices_of_execution)
-                self.assertDictEqual(metadata['measures'][str(index)],
-                                     {
-                                         'accuracy': index,
-                                         'summary': {
-                                             'time': index
-                                         }
-                                     })
+            _maybe_harvest(harvest_after_run, context, index_execution_id)
+            self._check_logs(context, index_execution_id,
+                             n_indices_of_execution,
+                             index)
+            self._check_output_files(context, index_execution_id, index)
+            self._check_measures(context, index_execution_id, index)
+            self._check_metadata_in_history(context, harvest_after_run, index,
+                                            index_execution_id,
+                                            n_indices_of_execution)
         return context, execution_id
 
-    def _check_execution_assignment(self, rainch, indices_per_execution,
-                                    indices_to_compositions):
+    def _check_metadata_in_history(
+            self, context: TestingContext, harvest_after_run: bool,
+            index: int,
+            index_execution_id: str,
+            n_indices_of_execution: int):
+        # When not harvesting after run, check that the history works
+        history = context.controller.get_history(
+            user=context.configuration.user,
+            project=context.configuration.project)
+        # If we harvested, the execution is in the history, we can check
+        # the metadata
+        if harvest_after_run:
+            metadata = json.loads(''.join(history))[index_execution_id]
+            # Check that the number of in
+            self.assertEqual(len(metadata['measures'].keys()),
+                             n_indices_of_execution)
+            self.assertDictEqual(metadata['measures'][str(index)],
+                                 {
+                                     'accuracy': index,
+                                     'summary': {
+                                         'time': index
+                                     }
+                                 })
+
+    def _check_measures(self, context: TestingContext, index_execution_id: str,
+                        index: int) -> None:
+        measures = json.loads(''.join(context.controller.get_measures(
+            index_execution_id, summary=False, index=index)))
+        self.assertDictEqual(measures, {
+            'accuracy': index,
+            'summary': {
+                'time': index
+            }})
+        summary_measures = json.loads(
+            ''.join(context.controller.get_measures(
+                index_execution_id, summary=True, index=index)))
+        self.assertDictEqual(summary_measures, {'time': index})
+
+    def _check_output_files(self, context: TestingContext,
+                            index_execution_id: str, index: int) -> None:
+        file_map = create_file_map_from_tarball(
+            context.controller.get_output_files(
+                index_execution_id, index=index, path=None))
+        self.assertDictEqual(file_map, {'the_file': f'index is: {index}'})
+
+    def _check_logs(self, context: TestingContext, index_execution_id: str,
+                    n_indices_of_execution: int,
+                    index: int) -> None:
+        logs_bytes = context.controller.get_logs(
+            index_execution_id, since=None)
+        logs_str = str(b''.join(logs_bytes), 'utf-8')
+        logs_lines = logs_str.split('\n')
+        # Make sure there's a newline at the end...
+        self.assertEqual(logs_lines[-1], '')
+        # ... and remove the empty line created by split
+        logs_lines = logs_lines[:-1]
+        self.assertEqual(len(logs_lines), n_indices_of_execution)
+        self.assertTrue(not all(
+            [line != str(index) for line in logs_lines]))
+
+    def _check_execution_assignment(
+            self, rainch: Tuple[int, int], indices_per_execution: int,
+            indices_to_compositions: Dict[str, dict]) -> None:
         range_len = rainch[1] - rainch[0]
         self.assertEqual(len(indices_to_compositions), range_len)
         number_of_different_executions = len(
@@ -161,3 +171,15 @@ class TestParallelIndices(unittest.TestCase):
                      for sc in indices_to_compositions.values())))
         self.assertEqual(math.ceil(range_len / (indices_per_execution or 1)),
                          number_of_different_executions)
+
+
+def _maybe_harvest(harvest_after_run: bool, context: TestingContext,
+                   index_execution_id: str):
+    execution_listing_status = get_execution_listing_status(
+        context.controller, index_execution_id)
+    while harvest_after_run and execution_listing_status is not None:
+        if execution_listing_status is not 'running':
+            context.controller.harvest()
+        time.sleep(0.1)
+        execution_listing_status = get_execution_listing_status(
+            context.controller, index_execution_id)
