7aac4d1c9f31c296fe7526b61d14a91510f39304
Sergio Giro <sergio@prodo.ai>
2019-02-01 15:52:17 +0000

    cli: remove unintended change in server_test.py

--- cli/test/test/plz/cli/server_test.py
+++ cli/test/test/plz/cli/server_test.py
@@ -25,7 +25,7 @@ class ServerTest(unittest.TestCase):
         latch = multiprocessing.Condition()
 
         def start_app():
-            cls.app.run()
+            cls.app.run(host='localhost', port=cls.port)
             with latch:
                 latch.notify()
 
