53fbea337e758583954b26b31c77b123930aa7f9
Somerset Ergiostohm <sergio@prodo.tech>
2019-02-11 08:51:33 +0000

    Tidying as to make sure we can deploy to AWS from scratch

--- test/end-to-end/logs/simple/main.py
+++ test/end-to-end/logs/simple/main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-import json
-import os.path
-
 print('foo')
 print('bar')
 print('baz')
--- vars.mk
+++ vars.mk
@@ -6,7 +6,16 @@ endif
 
 CONFIG_FILE = $(SECRETS_DIR)/config.json
 
-AMI_TAG = 2019-01-28
+
+this_file_dir := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+ami_tag_default := $(shell jq -r .ami_tag $(this_file_dir)/aws_config/config.json)
+
+ifeq ($(ami_tag_default),$(filter $(ami_tag_default),null ""))
+$(error 'Cannot find the default tag for the amis')
+endif
+
+# Get the ami tag from $CONFIG_FILE, otherwise use the default
+AMI_TAG = $(shell jq -r "if .ami_tag != null then .ami_tag else \"$(ami_tag_default)\" end" $(CONFIG_FILE))
 
 # The build timestamp is set in the secrets, and used when building the image
 # for the controller and the distribution for the cli. Uses:
