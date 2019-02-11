3b78b1de314f0a8ff5b76a0a2d5def8b8ef79b4c
Somerset Ergiostohm <sergio@prodo.tech>
2019-02-11 09:35:53 +0000

    machines/ml: Fix: Open only port 22 for the controller

--- machines/ml/main.tf
+++ machines/ml/main.tf
@@ -103,7 +103,7 @@ resource "aws_security_group" "plz_controller_ssh" {
   vpc_id = "${data.aws_vpc.main.id}"
 
   ingress {
-    from_port   = 0
+    from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
